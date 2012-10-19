d315b867f49eee488221c1e13dd0960e07f54091
Mireille Rollet <mireille.rollet@ac-lyon.fr>
2012-10-19 13:28:28 +0200

    suppression d'un commentaire

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once("include/fct.inc.php");
 require_once ("include/class.pdogsb.inc.php");
 include("vues/v_entete.php") ;
 session_start();
-$pdo = PdoGsb::getPdoGsb(); //connection à la base
+$pdo = PdoGsb::getPdoGsb(); 
 $estConnecte = estConnecte();
 if(!isset($_REQUEST['uc']) || !$estConnecte){
      $_REQUEST['uc'] = 'connexion';
